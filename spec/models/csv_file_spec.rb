4a70a62675f3410d3f3fd80db56a39afe393522e
jsalguero94 <jsalguero94@gmail.com>
2022-07-13 22:08:20 -0500

    CsvFile Enum Validation

--- spec/models/csv_file_spec.rb
+++ spec/models/csv_file_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe CsvFile, type: :model do
 
     it { is_expected.to validate_presence_of :name }
 
+    it { is_expected.to define_enum_for(:status).with_values([:'On Hold', :Processing, :Failed, :Finished]) }
+
     it 'has On Hold Status default value' do
       csv_file = build :csv_file
       expect(csv_file.status).to eq('On Hold')
