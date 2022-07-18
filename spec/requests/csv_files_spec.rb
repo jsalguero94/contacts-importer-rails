0263c153d89a1b7a9c01aee5a7bdae31302ff180
jsalguero94 <jsalguero94@gmail.com>
2022-07-18 12:19:16 -0500

    Request CsvFile tests

--- spec/requests/csv_files_spec.rb
+++ spec/requests/csv_files_spec.rb
@@ -1,25 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'CsvFiles', type: :request do
+  before do
+    new_user = create :user
+    sign_in new_user
+  end
+
   describe 'GET /index' do
     it 'render index'
   end
 
-  it 'upload a CsvFile and redirects to Home' do
-    get '/csv_files/new'
-    expect(response).to render_template(:new)
-
-    post '/csv_files', params: { csv_file: { csv: 'My Widget' } }
-
-    expect(response).to redirect_to('/')
-    follow_redirect!
-
-    expect(response).to render_template(:index)
-    expect(response.body).to include('Csv file was uploaded successfully.')
+  it 'new csv file' do
+    get new_csv_file_path
+    expect(response.body).to include 'Upload Csv File'
+    expect(response).to have_http_status(:ok)
   end
 
-  it 'does not render a different template' do
-    get '/csv_files/new'
-    expect(response).not_to render_template(:show)
+  it 'upload a CsvFile' do
+    csv_to_upload = Rack::Test::UploadedFile.new('spec/support/fixtures/contact1.csv')
+    post csv_files_path, params: { csv_file: { csv: csv_to_upload } }
+
+    follow_redirect!
+    expect(response.body).to include 'Csv file uploaded successfully.'
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'upload failed' do
+    csv_to_upload = Rack::Test::UploadedFile.new('spec/support/fixtures/contact1.txt')
+    post csv_files_path, params: { csv_file: { csv: csv_to_upload } }
+
+    expect(response.body).to include 'Csv must be a csv file'
+    expect(response).to have_http_status(:unprocessable_entity)
   end
 end
