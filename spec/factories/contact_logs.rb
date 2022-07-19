e69edd8b8351b1ecd7e5a0cc3c8a74c858da9e1c
jsalguero94 <jsalguero94@gmail.com>
2022-07-19 00:34:10 -0500

    bundle exec annotate

--- spec/factories/contact_logs.rb
+++ spec/factories/contact_logs.rb
@@ -1,3 +1,32 @@
+# == Schema Information
+#
+# Table name: contact_logs
+#
+#  id                   :bigint           not null, primary key
+#  address              :string
+#  cc_last_four_numbers :integer
+#  credit_card          :string
+#  credit_card_network  :string
+#  date_of_birth        :date
+#  email                :string
+#  error_message        :string
+#  name                 :string
+#  phone                :string
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  csv_file_id          :bigint           not null
+#  user_id              :bigint           not null
+#
+# Indexes
+#
+#  index_contact_logs_on_csv_file_id  (csv_file_id)
+#  index_contact_logs_on_user_id      (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (csv_file_id => csv_files.id)
+#  fk_rails_...  (user_id => users.id)
+#
 FactoryBot.define do
   factory :contact_log do
     name { 'Juan Pedro' }
