9ed793ea9544df32b8be24e6bd3aff6a3c73394d
jsalguero94 <jsalguero94@gmail.com>
2022-07-21 11:48:14 -0500

    action mailer removed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,6 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "contact_importer_production"
 
-  config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
