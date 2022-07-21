625500295fb757a5dbc636e374c9c3128106ed24
jsalguero94 <jsalguero94@gmail.com>
2022-07-21 13:03:48 -0500

    rails_best_practices

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,19 +7,23 @@ class ApplicationController < ActionController::Base
   def util_variables
     return unless user_signed_in?
 
-    contacts_counts
-    csv_counts
+    @stats = contacts_counts
+    @stats = @stats.merge csv_counts
   end
 
   def contacts_counts
-    @contacts_count = current_user.contacts.count
-    @contacts_failed_count = current_user.contact_logs.count
+    {
+      contacts_count: current_user.contacts.count,
+      contacts_failed_count: current_user.contact_logs.count
+    }
   end
 
   def csv_counts
-    @csv_on_hold = current_user.csv_files.On_Hold.count
-    @csv_processing = current_user.csv_files.Processing.count
-    @csv_finished = current_user.csv_files.Finished.count
-    @csv_failed = current_user.csv_files.Failed.count
+    {
+      csv_on_hold: current_user.csv_files.On_Hold.count,
+      csv_processing: current_user.csv_files.Processing.count,
+      csv_finished: current_user.csv_files.Finished.count,
+      csv_failed: current_user.csv_files.Failed.count
+    }
   end
 end
